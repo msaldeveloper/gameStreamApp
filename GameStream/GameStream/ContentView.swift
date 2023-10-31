5fd0672ccfeab1c608f7962b76307f402ad606b0
mario saldaña martinez <mariom@192.168.1.14>
2023-10-31 15:08:32 -0600

    creating session selector

--- GameStream/GameStream/ContentView.swift
+++ GameStream/GameStream/ContentView.swift
@@ -10,26 +10,56 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(red:19/255, green: 30/255, blue: 53/255, opacity: 1.0).ignoresSafeArea()
+            Color(red: 19 / 255, green: 30 / 255, blue: 53 / 255, opacity: 1.0).ignoresSafeArea()
             VStack {
-                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
+                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 40)
                 InicioYRegistroView()
             }
         }
     }
 }
 
-struct InicioYRegistroView : View {
-    var body : some View {
+struct InicioYRegistroView: View {
+    @State var selectorSession = true
+    var body: some View {
         VStack {
             HStack {
-                Text("INICIA SESIÓN")
-                Text("REGÍSTRATE")
+                Spacer()
+                Button("INICIA SESIÓN") {
+                     selectorSession = true
+                    print("inicio sesion boton presionado")
+                }
+                .foregroundColor(selectorSession ? .white : .gray)
+                Spacer()
+                Button("REGÍSTRATE") {
+                    self.selectorSession = false
+                    print("registro boton presionado")
+                    print(selectorSession)
+                }
+                .foregroundColor(selectorSession ? .gray : .white)
+                Spacer()
+            }
+            Spacer()
+            if selectorSession == true {
+                InicioSesionView()
+            }else {
+                RegistroView()
             }
             
         }
     }
-    
+}
+
+struct InicioSesionView: View {
+    var body: some View {
+        Text("soy la vista de inicio de sesion")
+    }
+}
+
+struct RegistroView: View {
+    var body: some View {
+        Text("Soy la vista de registro")
+    }
 }
 
 struct Pantalla01: View {
@@ -37,10 +67,11 @@ struct Pantalla01: View {
         Image("pantalla01")
     }
 }
-#Preview("pantalla01"){
+
+#Preview("pantalla01") {
     Pantalla01()
 }
-#Preview("miPantalla01"){
+
+#Preview("miPantalla01") {
     ContentView()
 }
-
