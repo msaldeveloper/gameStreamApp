d80a4a8bb21cfbca58e677b84c193f3335308660
msaldeveloper <msalda53@gmail.com>
2023-11-01 14:46:22 -0600

    add new profile image

--- GameStream/GameStream/RegisterFormView.swift
+++ GameStream/GameStream/RegisterFormView.swift
@@ -17,11 +17,15 @@ struct RegisterFormView: View {
                 Section{
                     Text("Elije una foto de perfil").foregroundStyle(Color(.white)).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     Text("puedes cambiarla o elegirla mas tarde").font(.caption).foregroundStyle(Color(.gray))
+                    
                     Button {
                         changeProfilePhoto()
                     } label: {
                         ZStack{
                             Image("profilePhoto")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80, height: 80)
                             Image("cameraIcon")
                             Circle()
                                 .stroke(.white, lineWidth: 2)
